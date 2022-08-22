c8d1ab92260ef441f8654b1271c651f92f21bbbf
WooYoon-local <dndbs14@gmail.com>
2022-08-23 03:14:47 +0900

    Feat: get new todo input from input textfield

--- to-do-list-app/src/components/todoList/todo_list.jsx
+++ to-do-list-app/src/components/todoList/todo_list.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import style from '../todoList/todo.module.css';
+import headerImage from '../todoList/main_img.png';
+import TodoInputForm from '../todo_input_form/todo_input_form';
 const TodoList = (props) => {
     const navigate = useNavigate();
     useEffect(() => {
@@ -9,7 +11,13 @@ const TodoList = (props) => {
         }
       }, []); 
     return(
-        <h1> TodoList Component</h1>
+      <div className={style.todo}>
+        <header className={style.header}>
+        <h1> To do List </h1>
+        <img src={headerImage} className={style.headerImage}></img>
+        </header>
+        <TodoInputForm />
+      </div>
     )
 }
 
